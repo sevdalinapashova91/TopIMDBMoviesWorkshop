fbfef9e74c910389e430431f0c3a32c40e12ef06
sevdalinapashova91 <sevdalina.pashova@yahoo.com>
2016-12-23 15:51:20 +0200

    make movie-short attribute for tr

--- app/movies-list/movie-short.component.ts
+++ app/movies-list/movie-short.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Movie } from '../core/movie.model';
 
 @Component({
-    selector: 'mvdb-movie-short',
+    selector: '[mvdb-movie-short]',
     templateUrl: `
         <td>
             <img [src]="movie.Poster" [alt]="movie.Title">
